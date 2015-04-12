440fe5dd97252d428f9607ce2acb4f61cdf60d3b
PjCox <philipcox83@gmail.com>
2015-04-12 17:18:31 +0100

    small change to xhr console log

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click' , function() {
-    var obj = roadRunner.getRunning('files/json/message.json');
-    console.log(obj);
+    roadRunner.getRunning('files/json/message.json');
+    
   });
 
 })();
